edb3aed653ef4d3f3036d702c48c3b466eddef7e
inficoder <inficoder@gmail.com>
2021-04-12 19:12:55 +0800

    day 12 - 03

--- src/main/java/com/buaa/review/algorithm/leetc_algorithm/code.md
+++ src/main/java/com/buaa/review/algorithm/leetc_algorithm/code.md
@@ -1,29 +1,9 @@
-public int numWays(int n, int k){
-    if(n == 0) return 0;
-    if(n == 1) return k;
-    return (numWays(n - 1) * (k - 1)) + (numsWays(n - 2) * (k - 1));
-}
-
-public static int[] compareNums(String str, char c){
-    int[] answer = new int[str.length()];
-    Arrays.fill(answer, length);
-    for(int i = 0; i < answer.length; i ++){
-        if(str.charAt(i) == c){
-            answer[i] == 0;
-            continue;
-        }
-        if(i != 0)
-            answer[i] = Math.min(answer[i], answer[i - 1] + 1);
+public int climbStairs(int n){
+    int i = 1;
+    int j = 2;
+    while(n > 2){
+        i ,j = j, i + j;
+        n --;
     }
-    
-    for(int i = answer.length - 1; i >= 0; i --){
-        if(str.charAt(i) == c){
-            answer[i] == 0;
-            continue;
-        }
-        if(i != answer.length - 1)
-            answer[i] = Math.min(answer[i], answer[i + 1] + 1);
-    }
-    
-    return answer;
+    System.out:.println(j);
 }
--- src/main/java/com/buaa/review/algorithm/leetc_algorithm/lc2104/day12.java
+++ src/main/java/com/buaa/review/algorithm/leetc_algorithm/lc2104/day12.java
@@ -182,11 +182,55 @@ public class day12 {
         return answer;
     }
 
-    public static void main(String[] args) {
-        int[] ints = shortestToChar("loveleetcode", 'e');
-        for (int anInt : ints) {
-            System.out.println(anInt);
+    /**
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     *
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     *
+     * 注意：给定 n 是一个正整数。
+     * 示例 1：
+     *
+     * 输入： 2
+     * 输出： 2
+     * 解释： 有两种方法可以爬到楼顶。
+     * 1.  1 阶 + 1 阶
+     * 2.  2 阶
+     * 示例 2：
+     *
+     * 输入： 3
+     * 输出： 3
+     * 解释： 有三种方法可以爬到楼顶。
+     * 1.  1 阶 + 1 阶 + 1 阶
+     * 2.  1 阶 + 2 阶
+     * 3.  2 阶 + 1 阶
+     */
+    // 递归 慢！
+    public static int climbStairs(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    // 递推
+    public static int climbStairs2(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int i = 1;
+        int j = 2;
+        int temp;
+        while(n > 2){
+            temp = i + j;
+            i = j;
+            j = temp;
+            n --;
         }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int i = climbStairs(5);
+        System.out.println(i);
 
     }
 }
